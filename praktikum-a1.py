98c17745382e8a70a71e6b7c1bab2cbc2738e9e1
foirteen <153788573+foirteen@users.noreply.github.com>
2023-12-14 10:50:40 +0800

    Add files via upload
    Praktikum a1

---
+++ praktikum-a1.py
@@ -0,0 +1,96 @@
+print("praktikum-1")
+
+# Print Nama Lengkap, NIM, Prodi
+print("Nama lengkap : A.haikal Fikri")
+print("NIM          : 231031016")
+print ("Prodi       : Sistem Informasi")
+print()
+x=2023
+
+print('angkatan',x)
+
+print()
+print()
+
+a=10
+print('data adalah:',a)
+print('tipe data  adalah',type(a))
+
+b=10.0
+print('data adalah:',b)
+print('tipe data  adalah',type(b))
+
+print()
+
+kampus= 'insititut teknologi habibie'
+print('data adalah:',kampus)
+print('tipe data kampus adalah',type(kampus))
+
+print()
+print()
+
+c=complex(2,3)
+print('data adalah:',c)
+print('tipe data  adalah',type(c))
+print()
+
+log = True
+print('data adalah:',log)
+print('tipe data  adalah',type(log))
+print()
+p=20
+q=23
+hasil= p + q
+print('hasil',p,'+',q,'=',hasil)
+
+
+# OPERASI ARITMATIKA
+
+#Penjumlahan
+a = 10
+b = 10
+buah = a + b #
+print(buah)
+
+#pengurangan
+pinjam = 5000
+uang = 2500
+sisaUang = pinjam - uang #
+print("sisaUang Anda adalah", sisaUang)
+
+#perkalian
+a = 2000
+b = 1000
+C = a * b #
+print("C hasilnya adalah", C)
+
+#pembagian
+baju = 20
+anak = 2
+hasil = baju / anak
+print("setiap anak akan memiliki baju sebanyak ", hasil) #
+
+#modulus
+
+a1 = 15
+a2 = 2
+hasil = a1 % a2
+print("Sisa bagi dari bilangan ", a1, "dan", a2,  " adalah", hasil)
+
+#pangkat
+a3 = 8
+a4 = 2
+hasilpangkat = a3 ** a4
+print(hasilpangkat)
+
+#pembagianbulat
+print(20//3)
+
+
+
+
+
+
+
+
+
